430e289c4731d52e6728c4a212ac530cfa28e334
daniellu <daniel12011201@gmail.com>
2021-06-28 01:29:36 +0800

    implement reverse-list

---
+++ cpp/206.reverse-linked-list.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=206 lang=cpp
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+
+        ListNode* cur = nullptr;
+        ListNode* buff;
+
+        while(head)
+        {
+            buff = head;
+            head = head->next;
+            buff->next = cur;
+            cur = buff;
+        }
+
+        return buff;
+    }
+};
+// @lc code=end
+
